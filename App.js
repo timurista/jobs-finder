fe7c4d962f9dd42b8ad7d13cc6b4eb36a7b6b81b
Tim Urista <timothy.urista@gmail.com>
2019-01-07 06:30:19 -0800

    add sub screens and navigation options for review stack

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import React from "react";
-import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import {
+  createStackNavigator,
+  createBottomTabNavigator,
+  createAppContainer
+} from "react-navigation";
 import AuthScreen from "./src/screens/AuthScreen";
 import WelcomeScreen from "./src/screens/WelcomeScreen";
+import MapScreen from "./src/screens/MapScreen";
+import DeckScreen from "./src/screens/DeckScreen";
+import SettingsScreen from "./src/screens/SettingsScreen";
+import ReviewScreen from "./src/screens/ReviewScreen";
+
+const ReviewStack = createStackNavigator({
+  review: ReviewScreen,
+  settings: SettingsScreen
+});
+
+ReviewStack.navigationOptions = {
+  title: "Review Jobs"
+};
 
 const MainNavigator = createBottomTabNavigator({
   welcome: { screen: WelcomeScreen },
-  auth: { screen: AuthScreen }
+  auth: { screen: AuthScreen },
+  main: {
+    screen: createBottomTabNavigator({
+      map: { screen: MapScreen },
+      deck: { screen: DeckScreen },
+      review: {
+        screen: ReviewStack
+      }
+    })
+  }
 });
 
 const App = createAppContainer(MainNavigator);
