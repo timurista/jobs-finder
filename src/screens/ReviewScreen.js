9abc456ecf759f40417b9e9750dd64c2d9df0321
Tim Urista <timothy.urista@gmail.com>
2019-01-08 06:29:16 -0800

    add navigation to settings screen in review screen

--- src/screens/ReviewScreen.js
+++ src/screens/ReviewScreen.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import { Button } from "react-native-elements";
 
 export default class ReviewScreen extends Component {
-  static navigationOptions = {
+  static navigationOptions = ({ navigation }) => ({
     title: "Review Jobs",
-    headerRight: <Text>Go Right</Text>
-  };
+    headerStyle: { marginTop: 10 },
+    headerRight: (
+      <Button
+        title="Settings"
+        onPress={() => {
+          navigation.navigate("settings");
+        }}
+        backgroundColor="rgba(0,0,0,0)"
+        color="rgba(0,122,255,1)"
+        fontSize={12}
+      />
+    )
+  });
 
   render() {
     return (
-      <View>
+      <View style={{ marginTop: 10 }}>
         <Text>ReviewScreen</Text>
         <Text>ReviewScreen</Text>
       </View>
