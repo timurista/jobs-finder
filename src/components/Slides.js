a17fbada369fc8d13810909870eb53a979be3e18
Tim Urista <timothy.urista@gmail.com>
2019-01-09 06:03:18 -0800

    add on complete hookup to take user to auth screen

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 import { View, Text, ScrollView, Dimensions } from "react-native";
+import { Button } from "react-native-elements";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default class WeclomeScreen extends Component {
+  renderLastSlide(i) {
+    if (i === this.props.data.length - 1) {
+      return (
+        <Button
+          buttonStyle={styles.buttonStyles}
+          title="Onwards!"
+          raised
+          onPress={this.props.onComplete}
+        />
+      );
+    }
+  }
+
   renderSlides() {
-    return this.props.data.map(slide => {
+    return this.props.data.map((slide, i) => {
       console.log("RENDERING SLIDES", slide);
       return (
         <View
@@ -13,6 +27,7 @@ export default class WeclomeScreen extends Component {
           style={[styles.slideStyle, { backgroundColor: slide.color }]}
         >
           <Text style={styles.textStyle}>{slide.text}</Text>
+          {this.renderLastSlide(i)}
         </View>
       );
     });
@@ -40,5 +55,9 @@ const styles = {
     alignItems: "center",
     alignContent: "center",
     alignSelf: "center"
+  },
+  buttonStyles: {
+    backgroundColor: "#0288D1",
+    marginTop: 20
   }
 };
